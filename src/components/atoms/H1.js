6d14a65a4392280c7337d47644a8a7a0472b3221
Paweł Miłczak <jogobello4@gmail.com>
2020-04-01 20:18:44 +0200

    random commit

--- src/components/atoms/H1.js
+++ src/components/atoms/H1.js
@@ -11,11 +11,6 @@ export default styled.h1`
   text-align: ${({ center }) => (center ? "center" : "left")};
   margin-bottom: 60px;
 
-  span {
-    color: ${({ theme, orange }) =>
-      orange ? theme.color.white : theme.color.orange}};
-  }
-
   @media ${({ theme }) => theme.device.laptop} {
     font-size: ${({ theme }) => theme.fontSize.xl};
   }
